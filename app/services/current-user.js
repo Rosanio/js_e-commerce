aae69a63c465cffd4717b7fbf70fc0bb190aaa52
Abigail Rolling and Matt Rosanio <arolling@gmail.com>
2016-04-06 13:41:18 -0700

    Add working login functionality

--- app/services/current-user.js
+++ app/services/current-user.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Service.extend({
-  client: "Matt",
+  client: {username: "Matt", password: "132"},
 
   logIn(user){
     this.set('client', user);
